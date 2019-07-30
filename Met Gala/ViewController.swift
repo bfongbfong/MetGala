d4ed4aee02d5dfe8f4a3e70aff13b18e3a64e7bb
En Kuang Fong <bfongbfong@gmail.com>
2019-07-30 19:27:21 -0400

    Initial Commit

---
+++ Met Gala/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Met Gala
+//
+//  Created by Brandon Fong on 7/30/19.
+//  Copyright © 2019 Fiesta Togo Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
